996fabe583b65c506d3a1aa6fb2ab3e9330409bf
faouzizi <elmbarkif@gmail.com>
2021-08-19 08:25:30 +0200

    respect some pep8 rules and add the right url on README

--- README.md
+++ README.md
@@ -3,4 +3,6 @@
 First of all, you need to install all dependencies using pip install -r requirements.txt (or equivalent).
 Then you can clone the project and run it. 
 
+Finally, you can go to http://127.0.0.1:5000/contactus and check your work.
+
 Enjoy :)
--- main.py
+++ main.py
@@ -11,22 +11,24 @@ import pandas as pd
 app = Flask(__name__)
 app.secret_key = 'dev fao football app'
 
+
 ###############################################
 #       Render Contact page                   #
 ###############################################
-@app.route('/contactus', methods=["GET","POST"])
+@app.route('/contactus', methods=["GET", "POST"])
 def get_contact():
     form = ContactForm()
     if request.method == 'POST':
-        name =  request.form["name"]
+        name = request.form["name"]
         email = request.form["email"]
         subject = request.form["subject"]
         message = request.form["message"]
-        res = pd.DataFrame({'name':name, 'email':email, 'subject':subject ,'message':message}, index=[0])
+        res = pd.DataFrame({'name': name, 'email': email, 'subject': subject, 'message': message}, index=[0])
         res.to_csv('./contactusMessage.csv')
         return render_template('contact.html', form=form)
     else:
         return render_template('contact.html', form=form)
+
 
 ###############################################
 #                Run app                      #
